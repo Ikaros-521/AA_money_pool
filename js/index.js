b7098b21437aa6909b31643d4f247fdabf030b5c
Ikaros <327209194@qq.com>
2022-01-21 17:27:28 +0800

    增加加入AA事件页面和功能，优化UI

--- js/index.js
+++ js/index.js
@@ -47,9 +47,12 @@ function browserRedirect() {
 	}
 }
 
+var event_id = 0;
+var username = "A";
+
 // 显示页面
-function showPage(pageId) {
-    var idArr = ["homepage_div", "add_div", "use_div"];
+function showPage(pageId, data) {
+    var idArr = ["homepage_div", "add_div", "use_div", "join_div"];
 
     for(var i = 0; i < idArr.length; i++) {
         if(idArr[i] == pageId) {
@@ -68,7 +71,31 @@ function showPage(pageId) {
             console.log("index:" + index);
             useAA(index);
         });
+    } else if(pageId == "join_div") {
+        event_id = data;
+        console.log("event_id:" + event_id);
+        $("#event_name_join_div").html(event_list["event"][event_id].name);
+        $("#all_money_join_div").html(event_list["event"][event_id].all_money);
+        $("#once_money_join_div").html(event_list["event"][event_id].once_money);
+        $("#end_time_join_div").html(event_list["event"][event_id].end_time);
+
+        $('#storeBtn_join_div').on('click', function(){
+            var data = {name: $("#name_join_div").val(), use: 0};
+            event_list["event"][event_id]["participants"].push(data);
+            event_list["event"][event_id]["all_money"] = int(event_list["event"][event_id]["all_money"]) + 
+                int(event_list["event"][event_id].once_money);
+            console.log("加入成功!");
+            show_hide_tip("成功加入 " + event_list["event"][event_id].name, 3000);
+            console.log(event_list);
+        });
     }
+}
+
+// 显示隐藏tip_div 提示框 str为显示的字符串,time为显示时长
+function show_hide_tip(str, time) {
+    $("#tip_div").show(1000);
+    $("#tip_span").text(str);
+    setTimeout(function(){$("#tip_div").hide(1000);}, time);
 }
 
 // 显示已经存在的AA事件列表
@@ -81,18 +108,32 @@ function showEventList() {
         var event_li = document.createElement("div");
         var ico = document.createElement("img");
         var event_name = document.createElement("span");
+        var once_money = document.createElement("span");
+        var all_money = document.createElement("span");
 
         ico.src = "../img/" + event_list["event"][i].ico;
         ico.style.width = "30%";
         event_name.innerHTML = event_list["event"][i].name;
+        event_name.style.width = "30%";
+        once_money.innerHTML = "单存" + event_list["event"][i].once_money;
+        once_money.style.width = "20%";
+        all_money.innerHTML = "总额" + event_list["event"][i].all_money;
+        all_money.style.width = "20%";
 
         event_li.id = event_list["event"][i].id;
         event_li.style.border = "1px solid #cfcfcf";
+        event_li.style.borderRadius = "3px";
+        event_li.style.height = "20%";
+        event_li.style.display = "flex";
+
         event_li.appendChild(ico);
         event_li.appendChild(event_name);
+        event_li.appendChild(once_money);
+        event_li.appendChild(all_money);
+
         event_li.onclick = function() {
             console.log(this.id);
-            joinAA(this.id);
+            showPage("join_div", this.id);
         };
 
         $("#event_list").append(event_li);
@@ -101,14 +142,24 @@ function showEventList() {
     }
 }
 
-// 加入AA事件
-function joinAA(id) {
-    console.log(id);
-}
-
 // 使用AA蓄钱池
 function useAA(id) {
     console.log(id);
+
+    $("#surplus_money_use_div").html(event_list["event"][id].surplus_money);
+    
+    var use_money = 0;
+    for(var i = 0; i < event_list["event"][id]["participants"].length; i++) {
+        console.log("name[" + i + "]:" + event_list["event"][id]["participants"][i].name);
+
+        if(event_list["event"][id]["participants"][i].name == username) {
+            use_money = event_list["event"][id]["participants"][i].use;
+            
+            break;
+        }
+    }
+
+    $("#use_money_use_div").html(use_money);
 }
 
 // 发起AA
@@ -131,4 +182,8 @@ function launchAA() {
     };
 
     event_list["event"].push(data);
+
+    show_hide_tip("成功发起 " + event_name, 3000);
+
+    showPage("homepage_div");
 }
